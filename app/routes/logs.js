b13301db80e39abf0dc4cd419ddcb7efd8b58516
mohan-kumar-selvaraj <mohankumars2020cse@gmail.com>
2024-09-16 12:15:34 +0530

    Paginaion + Controller (State Manage) + Route

--- app/routes/logs.js
+++ app/routes/logs.js
@@ -1,12 +1,21 @@
-// app/routes/logs.js
 import Route from '@ember/routing/route';
 
 export default class LogsRoute extends Route {
-    async model() {
-        const response = await fetch(
-            'http://localhost:8080/EventLogJNI/json',
-        );
-        const data = await response.json();
-        return data.logs;
+    queryParams = {
+        page: { refreshModel: true },
+        pageSize: { refreshModel: true }
+    };
+
+    async model(params) {
+        let page = params.page || 1; // Default to page 1 if undefined
+        let pageSize = params.pageSize || 10; // Default to pageSize 10 if undefined
+
+        let response = await fetch(`http://localhost:8080/EventLogJNI/json?page=${page}&pageSize=${pageSize}`);
+        let data = await response.json();
+
+        return {
+            logs: data.logs,
+            totalPages: data.totalPages
+        };
     }
 }
