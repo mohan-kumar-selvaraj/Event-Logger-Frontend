67f17be7f6e89d82160587a994792edc8445cb53
mohan-kumar-selvaraj <mohankumars2020cse@gmail.com>
2024-09-18 12:00:47 +0530

    Removed the unnecessary codes (continue)

--- app/routes/logs.js
+++ app/routes/logs.js
@@ -3,7 +3,6 @@ import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
 export default class LogsRoute extends Route {
-  @service session;
   @service router;
 
   async beforeModel() {
