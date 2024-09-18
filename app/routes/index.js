68bb69c3190ba84e6669575840ac33d50800c63b
mohan-kumar-selvaraj <mohankumars2020cse@gmail.com>
2024-09-18 11:12:31 +0530

    Redirect the endpoint / to /login

---
+++ app/routes/index.js
@@ -0,0 +1,11 @@
+// app/routes/index.js
+
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class IndexRoute extends Route {
+    @service router;
+    beforeModel() {
+        this.router.replaceWith('login'); // Correct usage of replaceWith
+    }
+}
